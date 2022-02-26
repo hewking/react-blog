54119f6111b6b954892ad9aaec8f03d8f8114b86
hewking <cjh1095358798@gmail.com>
2022-02-26 17:33:15 +0800

    feat: add header

--- components/Header.js
+++ components/Header.js
@@ -5,11 +5,11 @@ import { Row, Col, Menu, Icon } from 'antd';
 const Header = () => {
   return <div className='header'>
     <Row type="flex" justify='center'>
-      <Col>
+      <Col xs={24} sm={24} md={10} lg={10} xl={10}>
         <span className='header-logo'>技术胖</span>
         <span className='header-text'>专注前端开发，每年 100 集免费视频。</span>
       </Col>
-      <Col>
+      <Col xs={0} sm={0} md={14} lg={8} xl={6}>
         <Menu mode='horizontal'>
           <Menu.Item key="home"><Icon type="home" />首页</Menu.Item>
           <Menu.Item key="video"><Icon type="youtube" />视频</Menu.Item>
