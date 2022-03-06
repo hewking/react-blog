ed9d324913bfb7ef8e220ad8587011860892e263
hewking <cjh1095358798@gmail.com>
2022-03-06 14:54:00 +0800

    feature:更改Header icon style

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from '../static/style/components/header.module.css'
 import { Row, Col, Menu, Icon } from 'antd';
+import { CrownOutlined , YoutubeOutlined, HomeOutlined} from '@ant-design/icons';
 
 const Header = () => {
   return <div className={styles.header}>
@@ -10,10 +11,10 @@ const Header = () => {
         <span className={styles.header_text}>专注前端开发，每年 100 集免费视频。</span>
       </Col>
       <Col xs={0} sm={0} md={14} lg={8} xl={6}>
-        <Menu mode='horizontal'>
-          <Menu.Item key="home"><Icon type="home" />首页</Menu.Item>
-          <Menu.Item key="video"><Icon type="youtube" />视频</Menu.Item>
-          <Menu.Item key="life"><Icon type="smile" />生活</Menu.Item>
+        <Menu mode='horizontal' className={styles.menu}>
+          <Menu.Item key="home" className={styles.ant_menu_item}><HomeOutlined className={styles.header_menu_icon}/>首页</Menu.Item>
+          <Menu.Item key="video" className={styles.ant_menu_item}><YoutubeOutlined className={styles.header_menu_icon}/>视频</Menu.Item>
+          <Menu.Item key="life" className={styles.ant_menu_item}><CrownOutlined className={styles.header_menu_icon}/>生活</Menu.Item>
         </Menu>
       </Col>
     </Row>
