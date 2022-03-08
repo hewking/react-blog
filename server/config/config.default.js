45660f8c6dafe6c2cc0dbcb5de25ccae9b4fe930
hewking <cjh1095358798@gmail.com>
2022-03-08 15:54:02 +0800

    feature: rename to server, 配置mysql

--- server/config/config.default.js
+++ server/config/config.default.js
@@ -28,3 +28,23 @@ module.exports = appInfo => {
     ...userConfig,
   };
 };
+
+exports.mysql = {
+  // database configuration
+  client: {
+    // host
+    host: 'mysql.com',
+    // port
+    port: '3306',
+    // username
+    user: 'test_user',
+    // password
+    password: 'test_password',
+    // database
+    database: 'test',    
+  },
+  // load into app, default is open
+  app: true,
+  // load into agent, default is close
+  agent: false,
+};
