6f701d4f8511aed87f9e128c33ef29b1f1e8f9a6
hewking <cjh1095358798@gmail.com>
2022-03-14 15:33:25 +0800

    featur: add 查询文章列表

--- server/app/controller/default/home.js
+++ server/app/controller/default/home.js
@@ -3,13 +3,26 @@
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
-  
+
   async index() {
     const { ctx } = this;
-    let result = await this.app.mysql.get("blog_content", { title: '测试文章' });
-    let data = JSON.stringify(result);
-    console.log("index get mysql_result" + JSON.stringify(result));
-    ctx.body = data;
+    ctx.body = 'api hi！';
+  }
+
+  async getAriticleList() {
+    const { ctx } = this;
+
+    // 查询文章列表，通过左连接查询
+    const sql = 'SELECT a.id as id,' +
+      'a.title as title,' +
+      'a.introduce as introduce,' +
+      'a.addTime as addTime,' +
+      'a.view_count as view_count,' +
+      'b.typeName as typeName ' +
+      'FROM article a LEFT JOIN type b ON a.type_id = b.id';
+
+    const results = await ctx.app.mysql.query(sql);
+    ctx.body = { data: results };
   }
 
   async list() {
--- server/app/router/default.js
+++ server/app/router/default.js
@@ -7,4 +7,5 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/default/index', controller.default.home.index);
   router.get('/default/list', controller.default.home.list);
+  router.get('/default/getAriticleList', controller.default.home.getAriticleList);
 };
