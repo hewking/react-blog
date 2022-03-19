add8c59a935ccadccbec156dd9f262cdcc19b752
hewking <cjh1095358798@gmail.com>
2022-03-19 15:15:25 +0800

    feature: 增加admin react project and suport ts

---
+++ admin/src/App.tsx
@@ -0,0 +1,15 @@
+import {Button} from 'antd';
+import 'antd/dist/antd.less';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      </header>
+      <Button type="primary">Hello World</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ admin/src/pages/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Login from './Login.tsx';
+
+export default function Main(){
+  return (
+    <Router>
+      <Route path="/login/" component={Login} />
+    </Router>
+  )
+}
