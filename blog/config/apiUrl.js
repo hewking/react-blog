875659c2974a0303844c0321c4227a00d25b4711
hewking <cjh1095358798@gmail.com>
2022-03-28 10:00:39 +0800

    feature: update

--- blog/config/apiUrl.js
+++ blog/config/apiUrl.js
@@ -1,4 +1,5 @@
-let ipUrl = 'http://127.0.0.1:7001/default/';
+// let ipUrl = 'http://127.0.0.1:7001/default/';
+let ipUrl = 'http://119.29.195.26:7001/default/';
 
 let servicePath = {
   getArticleList: ipUrl + 'getAriticleList',
