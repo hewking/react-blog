0c6be2b55da7de015aa9b15fcf4c1f6f3b11b78b
jianhao <chengjianhao@fcbox.com>
2022-07-06 19:48:07 +0800

    feature: 增加eslint 配置

--- packages/server/app/router/admin.js
+++ packages/server/app/router/admin.js
@@ -1,13 +1,28 @@
-module.exports = app => {
+module.exports = (app) => {
   const { router, controller } = app;
   const adminauth = app.middleware.adminauth();
-  router.get('/admin/index',  controller.admin.main.index);
-  router.post('/admin/checkLogin', controller.admin.main.checkLogin);
-  router.get('/admin/getTypeInfo', adminauth, controller.admin.main.getTypeInfo);
-  router.post('/admin/addArticle', adminauth, controller.admin.main.addArticle);
-  router.post('/admin/updateArticle', adminauth, controller.admin.main.updateArticle);
-  router.get('/admin/getAriticleList', adminauth, controller.admin.main.getAriticleList);
-  router.get('/admin/deleteArticle/:id', adminauth, controller.admin.main.deleteArticle);
-  router.get('/admin/getArticleById/:id', controller.admin.main.getArticleById);
-
-}
+  router.get("/admin/index", controller.admin.main.index);
+  router.post("/admin/checkLogin", controller.admin.main.checkLogin);
+  router.get(
+    "/admin/getTypeInfo",
+    adminauth,
+    controller.admin.main.getTypeInfo
+  );
+  router.post("/admin/addArticle", adminauth, controller.admin.main.addArticle);
+  router.post(
+    "/admin/updateArticle",
+    adminauth,
+    controller.admin.main.updateArticle
+  );
+  router.get(
+    "/admin/getAriticleList",
+    adminauth,
+    controller.admin.main.getAriticleList
+  );
+  router.get(
+    "/admin/deleteArticle/:id",
+    adminauth,
+    controller.admin.main.deleteArticle
+  );
+  router.get("/admin/getArticleById/:id", controller.admin.main.getArticleById);
+};
--- packages/server/app/router/default.js
+++ packages/server/app/router/default.js
@@ -1,14 +1,20 @@
-'use strict';
+"use strict";
 
 /**
  * @param {Egg.Application} app - egg application
  */
-module.exports = app => {
+module.exports = (app) => {
   const { router, controller } = app;
-  router.get('/default/index', controller.default.home.index);
+  router.get("/default/index", controller.default.home.index);
   // 配置id query 参数
-  router.get('/default/getArticleById/:id', controller.default.home.getArticleById);
-  router.get('/default/getAriticleList', controller.default.home.getAriticleList);
-  router.get('/default/getTypeInfo', controller.default.home.getTypeInfo);
-  router.get('/default/getListById/:id', controller.default.home.getListById);
+  router.get(
+    "/default/getArticleById/:id",
+    controller.default.home.getArticleById
+  );
+  router.get(
+    "/default/getAriticleList",
+    controller.default.home.getAriticleList
+  );
+  router.get("/default/getTypeInfo", controller.default.home.getTypeInfo);
+  router.get("/default/getListById/:id", controller.default.home.getListById);
 };
